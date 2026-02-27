30f4e658c613698b6cb8da9397ff991c2c9c500b
Shiao Wang <109134157+wsasdsda@users.noreply.github.com>
2026-02-27 15:26:18 +0800

    Update run.sh

--- EvSLT/run.sh
+++ EvSLT/run.sh
@@ -5,7 +5,7 @@ CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch \
 --nproc_per_node=1 \
 --master_port=12678 \
 --use_env train_slt.py \
---batch-size 8 \
+--batch-size 6 \
 --num_workers 8 \
 --epochs 200 \
 --opt sgd \
